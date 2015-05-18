ca496bd145db7fd9737544a2e6517d565b7de86d
Rumpelteazer <carina-brandt@gmx.de>
2015-05-18 20:21:27 +0200

    Vorbedingungen in der Vormerkkarte eingefügt

--- Mediathek_Vorlage_Blatt06/src/de/uni_hamburg/informatik/swt/se2/mediathek/materialien/Vormerkkarte.java
+++ Mediathek_Vorlage_Blatt06/src/de/uni_hamburg/informatik/swt/se2/mediathek/materialien/Vormerkkarte.java
@@ -9,51 +9,80 @@ import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.medien.Medium;
  * @author Aurora
  *
  */
-public class Vormerkkarte {
-	private Kunde _ausleiher;
-	private List<Kunde> _vormerker;
-	private final Medium _medium;
+public class Vormerkkarte
+{
+    private Kunde _ausleiher;
+    private List<Kunde> _vormerker;
+    private final Medium _medium;
 
-	public Vormerkkarte(Medium medium) {
-		_vormerker = new LinkedList<Kunde>();
-		_medium = medium;
-	}
-	
-	public void wurdeAusgeliehen(Kunde ausleiher)
-	{
+    public Vormerkkarte(Medium medium)
+    {
+        _vormerker = new LinkedList<Kunde>();
+        _medium = medium;
+    }
+
+    /**
+     * 
+     * @param ausleiher
+     * 
+     * @require ausleiher != null
+     */
+    public void wurdeAusgeliehen(Kunde ausleiher)
+    {
+        assert ausleiher != null : "Vorbedingung verletzt: null";
         _ausleiher = ausleiher;
-	}
+    }
 
-	public void fuegeVormerkerHinzu(Kunde vormerker) {
-		if (_vormerker.size() < 3) {
-			_vormerker.add(vormerker);
-		}
-	}
+    /**
+     * 
+     * @param vormerker
+     * 
+     * @require vormerker != null
+     */
+    public void fuegeVormerkerHinzu(Kunde vormerker)
+    {
+        assert vormerker != null : "Vorbedingung verletzt: null";
+        if (_vormerker.size() < 3)
+        {
+            _vormerker.add(vormerker);
+        }
+    }
 
-	/**
-	 * 
-	 * @param ausleiher
-	 * @return ob Ausleihen moeglich ist
-	 * 
-	 * @require ausleiher != null
-	 */
-	public boolean pruefeObAusleihenMoeglich(Kunde ausleiher) {
-		return (_ausleiher == null && (_vormerker.get(0) == ausleiher || _vormerker.get(0) == null));
-	}
+    /**
+     * 
+     * @param ausleiher
+     * @return ob Ausleihen moeglich ist
+     * 
+     * @require ausleiher != null
+     */
+    public boolean pruefeObAusleihenMoeglich(Kunde ausleiher)
+    {
+        assert ausleiher != null : "Vorbedingung verletzt: null";
+        return (_ausleiher == null && (_vormerker.get(0) == ausleiher || _vormerker.get(0) == null));
+    }
 
-	public void entferneVormerker(Kunde vormerker) {
-		if (_vormerker.contains(vormerker)) {
-			_vormerker.remove(_vormerker.indexOf(vormerker));
-		}
-	}
-	
-	public void wurdeZurueckgegeben()
-	{
-		_ausleiher = null;
-	}
-	
-	public Medium getMedium()
-	{
-		return _medium;
-	}
+    /**
+     * 
+     * @param vormerker
+     * 
+     * @require vormerker != null
+     */
+    public void entferneVormerker(Kunde vormerker)
+    {
+        assert vormerker != null : "Vorbedingung verletzt: null";
+        if (_vormerker.contains(vormerker))
+        {
+            _vormerker.remove(_vormerker.indexOf(vormerker));
+        }
+    }
+
+    public void wurdeZurueckgegeben()
+    {
+        _ausleiher = null;
+    }
+
+    public Medium getMedium()
+    {
+        return _medium;
+    }
 }
