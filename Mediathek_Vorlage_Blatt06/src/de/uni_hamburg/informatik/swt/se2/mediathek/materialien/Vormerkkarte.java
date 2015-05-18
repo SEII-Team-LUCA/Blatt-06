1e9268ccb8826abe175fa55691a2bc20da65ae6b
Louis Kobras <4kobras@informatik.uni-hamburg.de>
2015-05-18 18:04:55 +0200

    update 2

---
+++ Mediathek_Vorlage_Blatt06/src/de/uni_hamburg/informatik/swt/se2/mediathek/materialien/Vormerkkarte.java
@@ -0,0 +1,48 @@
+package de.uni_hamburg.informatik.swt.se2.mediathek.materialien;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.medien.Medium;
+
+/**
+ * @author Aurora
+ *
+ */
+public class Vormerkkarte {
+	private Kunde _ausleiher;
+	private List<Kunde> _vormerker;
+	private final Medium _medium;
+
+	public Vormerkkarte(Kunde ausleiher, Medium medium) {
+		_ausleiher = ausleiher;
+		_vormerker = new LinkedList<Kunde>();
+		_medium = medium;
+	}
+
+	public void fuegeVormerkerHinzu(Kunde vormerker) {
+		if (_vormerker.size() < 3) {
+			_vormerker.add(vormerker);
+		}
+	}
+
+	public boolean pruefeObAusleihenMoeglich(Kunde ausleiher) {
+		return (_ausleiher == null && (_vormerker.get(0) == ausleiher || _vormerker.get(0) == null));
+	}
+
+	public void entferneVormerker(Kunde vormerker) {
+		if (_vormerker.contains(vormerker)) {
+			_vormerker.remove(_vormerker.indexOf(vormerker));
+		}
+	}
+	
+	public void wurdeZurueckgegeben()
+	{
+		_ausleiher = null;
+	}
+	
+	public Medium getMedium()
+	{
+		return _medium;
+	}
+}
